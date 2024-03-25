e823e25f49a47a094220e2f1b46603a0397c4006
arul1905 <116162459+arul1905@users.noreply.github.com>
2024-03-25 10:19:29 +0530

    Update index.php

--- Week1/index.php
+++ Week1/index.php
@@ -11,7 +11,7 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <!DOCTYPE html>
 <html>
 <head>
-    <title>c6ada7a9</title>
+    <title>arul</title>
     <?php require_once "bootstrap.php"; ?>
 </head>
 <body>
